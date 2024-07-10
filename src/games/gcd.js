4199799feacb85dc6f81696b867dd906a878a726
Serhio Bon <bssbss163@gmail.com>
2024-07-10 19:36:40 +0400

    change name of files

---
+++ src/games/gcd.js
@@ -0,0 +1,28 @@
+import getRandomInt from '../random.js';
+import engineGame from '../index.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (a, b) => {
+    let gcd = 1;
+    for (let i = 1; i <= a || i <= b; i += 1) {
+        if (a % i === 0 && b % i === 0) {
+            gcd = i;
+        }
+    }
+    return gcd;
+};
+
+const generateRound = () => {
+    const num1 = 40;
+    const num2 = 24;
+    const question = `${num1} ${num2}`;
+    const correctAnswer = getGcd(num1, num2).toString();
+    return [question, correctAnswer];
+};
+
+const startGame = () => {
+    engineGame(rules, generateRound);
+};
+
+export default startGame;
