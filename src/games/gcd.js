e6e2c8d6d47871379b03d020f8127d3b07ab717a
Serhio Bon <bssbss163@gmail.com>
2024-07-10 20:08:21 +0400

    fix errors from linter

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,25 +4,25 @@ import engineGame from '../index.js';
 const rules = 'Find the greatest common divisor of given numbers.';
 
 const getGcd = (a, b) => {
-    let gcd = 1;
-    for (let i = 1; i <= a || i <= b; i += 1) {
-        if (a % i === 0 && b % i === 0) {
-            gcd = i;
-        }
+  let gcd = 1;
+  for (let i = 1; i <= a || i <= b; i += 1) {
+    if (a % i === 0 && b % i === 0) {
+      gcd = i;
     }
-    return gcd;
+  }
+  return gcd;
 };
 
 const generateRound = () => {
-    const num1 = 40;
-    const num2 = 24;
-    const question = `${num1} ${num2}`;
-    const correctAnswer = getGcd(num1, num2).toString();
-    return [question, correctAnswer];
+  const num1 = getRandomInt(0, 100);
+  const num2 = getRandomInt(0, 100);
+  const question = `${num1} ${num2}`;
+  const correctAnswer = getGcd(num1, num2).toString();
+  return [question, correctAnswer];
 };
 
 const startGame = () => {
-    engineGame(rules, generateRound);
+  engineGame(rules, generateRound);
 };
 
 export default startGame;
