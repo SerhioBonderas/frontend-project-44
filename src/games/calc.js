ed4b252dc5fcf0362a4447e96d569013edfa7e4b
Serhio Bon <bssbss163@gmail.com>
2024-07-20 11:49:38 +0400

    add function to the games logic

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,26 +3,32 @@ import engineGame from '../index.js';
 
 const rules = 'What is the result of the expression?';
 
+const calculate = (num1, num2, operator) => {
+  let result = null;
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+  };
+  
+  return result;
+};
+
 const generateRound = () => {
   const arrOperators = ['+', '-', '*'];
   const operator = arrOperators[getRandomInt(0, 2)];
   const firstNum = getRandomInt(0, 50);
   const secondNum = getRandomInt(0, 50);
   const question = `${firstNum} ${operator} ${secondNum}`;
-  let answer = 0;
-  switch (operator) {
-    case '+':
-      answer = firstNum + secondNum;
-      break;
-    case '-':
-      answer = firstNum - secondNum;
-      break;
-    case '*':
-      answer = firstNum * secondNum;
-      break;
-    default:
-  }
-  const correctAnswer = answer.toString();
+  let correctAnswer = calculate(firstNum, secondNum, operator).toString();
+
   return [question, correctAnswer];
 };
 
--- src/games/even.js
+++ src/games/even.js
@@ -3,9 +3,11 @@ import engineGame from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => number % 2 === 0;
+
 const generateRound = () => {
   const question = getRandomInt(1, 100);
-  const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
